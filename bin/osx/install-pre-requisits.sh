66bfbcbc9ec8f9242b0a5cdc39701d972780f762
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2018-08-22 23:49:27 +0100

    Trying to setup gaudi via a curlable script

--- bin/osx/install-pre-requisits.sh
+++ bin/osx/install-pre-requisits.sh
@@ -1,7 +1,7 @@
 printf "\nWe need to prepare your machine by install some required software\n\n"
 
 if ! type brew &> /dev/null; then
-    printf "We noticed that ${YELLOW}brew${NC} is not installed on your machine .. Installing now ... \n" -n 1;
+    printf "We noticed that brew is not installed on your machine .. Installing now ... \n" -n 1;
     # Install Homebrew .. a must !
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
@@ -9,27 +9,3 @@ fi
 printf "Installing cask and mas as they are highly recommended .. sorry it is not up to you to decide ;)\n"
 brew tap caskroom/cask
 brew install mas
-
-if [[ $SHELL_TYPE = "bash" ]]; then
-    if ! type bash &> /dev/null; then
-        read -p "We noticed that you selected bash as your shell but it was not installed .. do you wish to install it ? [Y/N] " -n 1;
-        echo "";
-        if [[ $REPLY =~ ^[yY]$ ]]; then
-            printf "\nInstalling bash shell"
-            brew install bash
-            sudo chsh -s /usr/local/bin/bash
-        fi;
-    fi
-fi;
-
-if [[ $SHELL_TYPE = "zsh" ]]; then
-    if ! type zsh &> /dev/null; then
-        read -p "We noticed that you selected zsh as your shell but it was not installed .. do you wish to install it ? [Y/N] " -n 1;
-        echo "";
-        if [[ $REPLY =~ ^[yY]$ ]]; then
-            printf "\nInstalling zsh shell"
-            brew install zsh
-            sudo chsh -s /usr/local/bin/zsh
-        fi;
-    fi
-fi;

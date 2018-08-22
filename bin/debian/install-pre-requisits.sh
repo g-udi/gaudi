52c4f270aaaadd18437c101ac142e08acd58c3a3
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2018-08-22 23:49:27 +0100

    Trying to setup gaudi via a curlable script

--- bin/debian/install-pre-requisits.sh
+++ bin/debian/install-pre-requisits.sh
@@ -1,4 +1,4 @@
-printf "\n${RED}We need to prepare your machine by install some required software${NC}\n\n"
+printf "\nWe need to prepare your machine by install some required software\n\n"
 
 # Updating apt to refresh repos
 sudo apt-get update
@@ -12,13 +12,13 @@ sudo apt-get curl
 sudo apt-get file
 
 if ! type git &> /dev/null; then
-    printf "We noticed that ${YELLOW}git${NC} is not installed on your machine .. Installing now ... \n" -n 1;
+    printf "We noticed that git is not installed on your machine .. Installing now ... \n" -n 1;
     # Install Homebrew .. a must !
     sudo apt-get install git-all
 fi
 
 if ! type brew &> /dev/null; then
-    printf "We noticed that ${YELLOW}brew${NC} is not installed on your machine .. Installing now ... \n" -n 1;
+    printf "We noticed that brew is not installed on your machine .. Installing now ... \n" -n 1;
     # Install Homebrew .. a must !
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
     test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
@@ -26,16 +26,3 @@ if ! type brew &> /dev/null; then
     test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
     echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
 fi
-
-if [[ $SHELL_TYPE = "zsh" ]]; then
-    if ! type zsh &> /dev/null; then
-        read -p "We noticed that you selected zsh as your shell but it was not installed .. do you wish to install it ? [Y/N] " -n 1;
-        echo "";
-        if [[ $REPLY =~ ^[yY]$ ]]; then
-            printf "\nInstalling ${RED}zsh shell${NC} shell"
-            sudo apt-get install zsh
-            sudo wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-            sudo chsh -s `which zsh`
-        fi;
-    fi
-fi;
---
+++ install.sh
@@ -0,0 +1,31 @@
+# Getting the operating system of the machine
+case `uname` in
+Linux )
+    LINUX=1
+    which yum && { export OS=centos; return; }
+    which zypper && { export OS=opensuse; return; }
+    which apt-get && { export OS=debian; return; }
+    ;;
+Darwin )
+    DARWIN=1
+    export OS=osx
+    ;;
+* );;
+esac    
+
+if [ ! -n "$GAUDI" ]; then
+    GAUDI=~/.gaudi
+fi
+
+# Run the installation pre-requisites based on each operating system defined in gaudi
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ahmadassaf/gaudi/tree/master/${OS}/install-pre-requisits.sh)"
+
+env git clone --depth=1 https://github.com/ahmadassaf/gaudi.git "$GAUDI" || {
+    printf "Error: Cloning of gaudi into this machine failed :(\n"
+    exit 1
+}
+
+. "$GAUDI/setup.sh"
+
+
+

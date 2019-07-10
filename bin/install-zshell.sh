2614daea2715e34876624656a571b41d7cd9cb9c
Michael Paterson <mike@beamery.com>
2019-07-10 19:26:28 +0100

    step not needed for mac

--- bin/install-zshell.sh
+++ bin/install-zshell.sh
@@ -5,11 +5,10 @@ if [[ $REPLY =~ ^[yY]$ ]]; then
       if [ ! -n $(which zsh) ]; then
             brew install zsh
             zsh_path=$(which zsh)
-            grep -Fxq "$zsh_path" /etc/shells || sudo bash -c "echo $zsh_path >> /etc/shells"
             sudo dscl . -create /Users/$USER UserShell zsh_path
       else
+            printf "You already have ZSH installed, its most likely the default zsh \n"
             if [ !$(brew list | grep zsh) ]; then
-                  printf "You already have ZSH installed, its most likely the default zsh \n"
                   read -p "Would you like to install with brew instead?"
                   if [[ $REPLY =~ ^[yY]$ ]]; then
                         brew install zsh

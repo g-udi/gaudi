c66dca18dc770239702667e911bc9127cb4507b9
Michael Paterson <mike@beamery.com>
2019-07-16 22:05:56 +0100

    using stow

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 read -p  "Would you like to install your dotfiles wtih stow?  " -n 1
+
 if [[ $REPLY =~ ^[yY]$ ]]; then
     if ! type stow &> /dev/null; then
         printf "\n\n We noticed that stow is not installed on your machine .. Installing now ... \n" -n 1;
@@ -9,8 +10,14 @@ if [[ $REPLY =~ ^[yY]$ ]]; then
         printf "stow is installed ✓\n\n\n"
     fi
 
-    if [ -d "$SOURCE_LOCATION/dotfiles" ]; then
-        printf "We have your dotfiles"
+    if [ -d "$SOURCE_LOCATION/gaudi-files/dotfiles" ]; then
+        for d in $SOURCE_LOCATION/gaudi-files/dotfiles/* ; do
+            if [ -d "$d" ]; then
+                file=`basename $d`
+                echo "$file"
+                stow $file --target="/Users/michael/"
+            fi
+        done
     fi
 
 fi;

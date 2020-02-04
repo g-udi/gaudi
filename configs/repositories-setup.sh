8c888f45d76a76e4415bdd0899a882528ac5803d
Ahmad A M Assaf <ahmadassaf@Ahmads-MacBook-Pro.local>
2020-02-04 16:08:40 +0000

    some cleaning up

--- configs/repositories-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "";
-read -p "Would you like to clone all of the development repositories? [Y/N] " -n 1;
-echo "";
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    curl -L -O https://raw.githubusercontent.com/SeedJobs/git-beam-it/master/git-beam-it && sudo mv git-beam-it /usr/local/bin/ && sudo chmod +x /usr/local/bin/git-beam-it
-    git beam-it -d '../' -t
-fi;

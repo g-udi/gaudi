c32f6fcdf49acd09ade3bc885f8362ebb522d8a4
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2022-06-09 17:44:52 +0100

    Trying to fix the templates clone

--- configs/configure-gaudi.sh
+++ configs/configure-gaudi.sh
@@ -6,7 +6,9 @@ declare GAUDI_DEFAULT_TEMPLATES GAUDI_TEMPLATES_LOCATION
 GAUDI_DEFAULT_TEMPLATES="https://github.com/g-udi/gaudi-templates"
 GAUDI_TEMPLATES_LOCATION="${HOME}/.gaudi/templates/"
 
-_echo "
+function _clone-gaudi-templates {
+
+    _echo "
 The next step will prompt you for the url of gaudi templates (lists, hooks, templates, etc.)...
 
 ${YELLOW}[D,d]${NC} will install g-udi's default templates from: ${MAGENTA} $GAUDI_DEFAULT_TEMPLATES
@@ -14,14 +16,18 @@ ${YELLOW}[D,d]${NC} will install g-udi's default templates from: ${MAGENTA} $GAU
 ${YELLOW}If you want to point to any other location then just type the github url of that repo${NC}
 "
 
-printf ">> "
-read -r GAUDI_TEMPLATE_URL;
-echo ""
+    printf ">> "
+    read -r GAUDI_TEMPLATE_URL;
+    echo ""
 
 
-if [[ $GAUDI_TEMPLATE_URL =~ ^[dD]$ ]]; then
-    GAUDI_TEMPLATE_URL=$GAUDI_DEFAULT_TEMPLATES
-fi;
+    if [[ $GAUDI_TEMPLATE_URL =~ ^[dD]$ ]]; then
+        GAUDI_TEMPLATE_URL=$GAUDI_DEFAULT_TEMPLATES
+    fi;
+
+    git clone "$GAUDI_TEMPLATE_URL" "$GAUDI_TEMPLATES_LOCATION"
+
+}
 
 if [[ -d $GAUDI_TEMPLATES_LOCATION ]]; then
     printf "${RED}%s${NC}\n\n%s" "We noticed that there already gaudi templates in $GAUDI_TEMPLATES_LOCATION" "Would you like to overwrite those? [Y/N] "
@@ -30,10 +36,12 @@ if [[ -d $GAUDI_TEMPLATES_LOCATION ]]; then
     echo "";
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         rm -rf "$GAUDI_TEMPLATES_LOCATION"
-        git clone $GAUDI_TEMPLATE_URL "$GAUDI_TEMPLATES_LOCATION"
+        _clone-gaudi-templates
     fi;
 else
-    /bin/mkdir "$HOME/.gaudi/templates"
-    git clone $GAUDI_TEMPLATE_URL "$GAUDI_TEMPLATES_LOCATION"
+    mkdir "$HOME/.gaudi/templates"
+    _clone-gaudi-templates
 fi
+
+
 

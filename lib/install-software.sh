01cf6b93df3cd64e9f38dd5aee51bb607f6ca9b5
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2023-06-12 18:54:34 +0100

    removing listtype check

--- lib/install-software.sh
+++ lib/install-software.sh
@@ -22,42 +22,39 @@ for item in "${softwareLists[@]}"; do
     
     if [[ $operatingSystem = "$OS" || $operatingSystem = "*" ]]; then
 
-        if command_exists "${listType}"; then
-            
-            # We need now to check if we need to run any pre hooks
-            find "$GAUDI_TEMPLATES_LOCATION" -type f -iname "pre.${listType}*.hooks.sh" | while read -r PRE_HOOK; do
-                . "$PRE_HOOK"
-            done
+        # We need now to check if we need to run any pre hooks
+        find "$GAUDI_TEMPLATES_LOCATION" -type f -iname "pre.${listType}*.hooks.sh" | while read -r PRE_HOOK; do
+            . "$PRE_HOOK"
+        done
 
-            for LIST in $(find "$GAUDI_TEMPLATES_LOCATION" -type f -iname "*${listType}.list.sh"); do
-                listName=$(cat "$LIST" | grep "# @List: ")
-                if [[ $LIST = *"default"* ]]; then
-                    printf "\n%b\n" "🤖 Installing ${YELLOW}default ${listType}${NC} software list"
-                else
-                    _listName=$(cat "$LIST" | grep "# @Name:")
-                    _listDescription=$(cat "$LIST" | grep "# @Description:")
-                    printf "\n%b\n" "🤖 Installing${YELLOW}${_listName#*:} ${listType}${NC} software list:${MAGENTA}${_listDescription#*:}${NC}"
-                fi;
+        for LIST in $(find "$GAUDI_TEMPLATES_LOCATION" -type f -iname "*${listType}.list.sh"); do
+            listName=$(cat "$LIST" | grep "# @List: ")
+            if [[ $LIST = *"default"* ]]; then
+                printf "\n%b\n" "🤖 Installing ${YELLOW}default ${listType}${NC} software list"
+            else
+                _listName=$(cat "$LIST" | grep "# @Name:")
+                _listDescription=$(cat "$LIST" | grep "# @Description:")
+                printf "\n%b\n" "🤖 Installing${YELLOW}${_listName#*:} ${listType}${NC} software list:${MAGENTA}${_listDescription#*:}${NC}"
+            fi;
 
-                printf "Would you like to proceed ? [Y/N] ";
+            printf "Would you like to proceed ? [Y/N] ";
+            if [[ $(read_answer) =~ ^[Yy]$ ]]; then
+                . "$LIST"
+                referencedList=$(echo "${listName#*:}" | xargs)
+                __list=$referencedList[@]
+                printf "Would you like to install all the recommended software [Type N to select what you want to install one by one]? [Y/N] ";
                 if [[ $(read_answer) =~ ^[Yy]$ ]]; then
-                    . "$LIST"
-                    referencedList=$(echo "${listName#*:}" | xargs)
-                    __list=$referencedList[@]
-                    printf "Would you like to install all the recommended software [Type N to select what you want to install one by one]? [Y/N] ";
-                    if [[ $(read_answer) =~ ^[Yy]$ ]]; then
-                        installSoftwareList "$listCommand" "false" "${!__list}"
-                    else
-                        installSoftwareList "$listCommand" "true" "${!__list}"
-                    fi
+                    installSoftwareList "$listCommand" "false" "${!__list}"
+                else
+                    installSoftwareList "$listCommand" "true" "${!__list}"
                 fi
-            done;
-            
-            # We need now to check if we need to run any post hooks
-            find "$GAUDI_TEMPLATES_LOCATION" -type f -iname "post.${listType}*.hooks.sh" | while read -r POST_HOOK; do
-                . "$POST_HOOK"
-            done
-        fi
+            fi
+        done;
+        
+        # We need now to check if we need to run any post hooks
+        find "$GAUDI_TEMPLATES_LOCATION" -type f -iname "post.${listType}*.hooks.sh" | while read -r POST_HOOK; do
+            . "$POST_HOOK"
+        done
     fi
 
 done

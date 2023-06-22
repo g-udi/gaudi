c8abfdc2a7e403f2778dcb9361bd048522143331
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2023-06-22 02:15:01 +0100

    Update configs.sh

--- bin/configs.sh
+++ bin/configs.sh
@@ -24,8 +24,11 @@ function mas {
 function npm {
     chip_type=$(sysctl -n machdep.cpu.brand_string)
     if [[ "$chip_type" == *Intel* ]]; then 
+        export PATH=$PATH:/usr/local/bin
         /usr/local/bin/npm "$@"
-    else /opt/homebrew/bin/npm "$@"
+    else
+        export PATH=$PATH:/opt/homebrew/bin
+        /opt/homebrew/bin/npm "$@"
     fi
 }
 

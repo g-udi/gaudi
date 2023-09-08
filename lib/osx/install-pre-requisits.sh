3d16dff2882f3ee01580757512c8da8ce0ec4faf
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2023-09-08 12:43:36 +0100

    Update install-pre-requisits.sh

--- lib/osx/install-pre-requisits.sh
+++ lib/osx/install-pre-requisits.sh
@@ -6,19 +6,12 @@ source "$SOURCE_LOCATION/lib/${OS}/config-sudo.sh"
 
 printf "\nWe need to prepare your machine by install some required software\n\n"
 
-if ! command_exists brew; then
-    printf "\n%s\n" "We noticed that brew is not installed on your machine .. Installing now ...";
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    printf "${RED}%s$GREEN %s ${NC}" "brew is installed" "✓"
-fi
 
-if ! command_exists mas; then
-    printf "\n%s\n" "Installing mas is highly recommended .. sorry it is not up to you to decide ;)"
-    brew install mas
-else
-    printf "${RED}%s$GREEN %s ${NC}" "mas is installed" "✓"
-fi
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+printf "${RED}%s$GREEN %s ${NC}" "brew is installed" "✓"
+
+brew install mas
+printf "${RED}%s$GREEN %s ${NC}" "mas is installed" "✓"
 
 if ! command_exists git; then
     printf "\n%s\n" "We noticed that git is not installed on your machine .. Installing now ...";
